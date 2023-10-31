c393fd71a5a0f64116107c97be42ce33fdd815a8
aceJacek <acejacek@gmail.com>
2023-10-31 15:49:48 +0100

    day 1

---
+++ day1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define INPUT_FILE "day1.txt"
+
+int main()
+{
+    FILE* input = fopen(INPUT_FILE, "r");
+    if (!input)
+    {
+        fprintf(stderr, "Cant load file.\n");
+        exit(EXIT_FAILURE);
+    }
+    int level = 0;
+    int basement = 0;
+    int position = 1;
+    while (1)
+    {
+        char c = fgetc(input);
+        if (feof(input)) break;
+
+        if (c == '(') level++;
+        else if (c == ')') level--;
+        if (basement == 0)
+            if (level < 0) basement = position;
+
+        position++; 
+    }
+    fclose(input);
+
+    printf("Level: %d\n", level);
+    printf("First position into basement: %d\n", basement);
+
+    return 0;
+}
