a77fad8ed5d4fda868ebf419e5b8f0dc304595f9
aceJacek <acejacek@gmail.com>
2023-10-31 17:30:01 +0100

    day2

--- day2.c
+++ day2.c
@@ -3,8 +3,6 @@
 
 #define INPUT_FILE "day2.txt"
 
-
-
 int main()
 {
     const char filename[] = INPUT_FILE;
@@ -20,29 +18,36 @@ int main()
     ssize_t read = 0;
 
     size_t total_area = 0;
+    size_t ribbon = 0;
 
     while ((read = getline(&line, &len, input)) != -1)
     {
         int l, w, h;
         sscanf(line, "%dx%dx%d", &l, &w, &h);
+
         int first = l * w;
         int second = w * h;
         int third = h * l;
-
-        
-
 
         int slack = first;
         if (slack > second) slack = second;
         if (slack > third) slack = third;
 
         total_area += 2 * first + 2 * second + 2 * third + slack;
+
+        int bow = l * w * h;
+        int longest = l;
+        if (longest < w) longest = w;
+        if (longest < h) longest = h;
+
+        ribbon += bow + l + l + w + w + h + h - longest - longest;
     }
 
     fclose(input);
     free(line);
 
     printf("Total area of paper: %zd\n", total_area);
+    printf("Total length of ribbon: %zd\n", ribbon);
 
     return 0;
 }
